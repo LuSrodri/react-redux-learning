77c8f7e2562297be5405237a80b25817a9d5f05b
LuSrodri <lucassrodri08@gmail.com>
2022-01-25 15:00:20 -0300

    card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import Card from './components/card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Redux Learning</h1>
+      <div className='row'>
+        <Card title="Card #01" red> ONE</Card>
+        <Card title="Card #02" blue> TWO</Card>
+        <Card title="Card #03" green> Three</Card>
+        <Card title="Card #04" purple> Four</Card>
+      </div>
+      <div className='row'>
+      </div>
     </div>
   );
 }
